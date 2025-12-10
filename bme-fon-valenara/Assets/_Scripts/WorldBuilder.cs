c380fd5fedc8dd63be357047b6f1889b6d187733
Trenton Arndt <trenton360@hotmail.com>
2025-12-09 19:53:36 -0600

    Added UI menu to see the seed that is generated and update the seed to what you might want. PerlinNoise from unity sometimes has an issue where value is >1 and <0.  Clamp fixes this.

--- bme-fon-valenara/Assets/_Scripts/WorldBuilder.cs
+++ bme-fon-valenara/Assets/_Scripts/WorldBuilder.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 
+using TMPro;
+
 using UnityEngine;
 
 using Random = UnityEngine.Random;
@@ -12,16 +14,38 @@ public class WorldBuilder : MonoBehaviour
     public int Height;
     private List<GameObject> Cells;
 
-    public int RandomSeed;
 
+    public int RandomSeed;
+    public TMP_InputField SeedInputField;
+    public GameObject CellParent;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if (RandomSeed == 0)
+        GenerateWorld();
+    }
+
+    public void SetSeed()
+    {
+        var randomSeedValue = SeedInputField.text;
+
+        var hasSeed = int.TryParse(randomSeedValue, out RandomSeed);
+
+        if (!hasSeed)
         {
             RandomSeed = Random.Range(-1000000, 1000000);
         }
+
+        if (string.IsNullOrEmpty(SeedInputField.text))
+        {
+            SeedInputField.text = $"{RandomSeed}";
+        }
+    }
+
+    public void GenerateWorld()
+    {
+        ClearCellHolder();
+        SetSeed();
 
         Cells = new List<GameObject>();
         for (var w = 0; w < Width; w++)
@@ -30,14 +54,14 @@ public class WorldBuilder : MonoBehaviour
             {
                 var newCell = Instantiate(CellPrefab, new Vector3(w - Width / 2f, h - Height / 2f, 0), CellPrefab.transform.localRotation);
 
+                newCell.transform.SetParent(CellParent.transform);
+
                 var sx = (w + RandomSeed) / 20f;
                 var sy = (h + RandomSeed) / 20f;
+                //-477571
+                var perlinValue = Math.Clamp(Mathf.PerlinNoise(sx, sy), 0.0f, 1.0f);
 
-                var cellType = Math.Floor(Mathf.PerlinNoise(sx, sy) * 7.0f);
-
-                Debug.Log(cellType);
-
-                Debug.Log(Mathf.PerlinNoise((w + RandomSeed) / 20.0f, (h + RandomSeed) / 20.0f));
+                var cellType = Math.Floor(perlinValue * 7.0f);
 
                 switch (cellType)
                 {
@@ -69,6 +93,14 @@ public class WorldBuilder : MonoBehaviour
 
                 Cells.Add(newCell);
             }
+        }
+    }
+
+    private void ClearCellHolder()
+    {
+        foreach (Transform child in CellParent.transform)
+        {
+            Destroy(child.gameObject);
         }
     }
 
