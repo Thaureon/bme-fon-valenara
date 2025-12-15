a505c01c879a88f2707b522cd69ac774a0ad7d07
Trenton Arndt <trenton360@hotmail.com>
2025-12-14 22:13:03 -0600

    Got movement of character and world working so the player can move around and regenerating the world places the player back at the center. Generation is extremely slow since it is removing and readding everything every time.

--- bme-fon-valenara/Assets/_Scripts/PlayerMovementScript.cs
+++ bme-fon-valenara/Assets/_Scripts/PlayerMovementScript.cs
@@ -6,18 +6,43 @@ public class PlayerMovementScript : MonoBehaviour
     public GameObject Player;
     public Camera Camera;
 
+    public WorldBuilder WorldManager;
+
     private InputAction _moveAction;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _moveAction = InputSystem.actions.FindAction("Move");
+        _moveAction.performed += Move;
     }
 
     // Update is called once per frame
     void Update()
     {
-        var triggered = _moveAction.;
-        if()
+        //var triggered = _moveAction.;
+        //if()
+    }
+
+    public void MovePlayer(Vector2 newPosition)
+    {
+        var translation = newPosition - (Vector2)Player.transform.position;
+
+        Player.transform.Translate(translation);
+        Camera.transform.Translate(translation);
+    }
+
+    private void Move(InputAction.CallbackContext context)
+    {
+        var movement = (Vector2)context.ReadValueAsObject();
+
+        var translation = movement;
+        translation.x = Mathf.Round(translation.x);
+        translation.y = Mathf.Round(translation.y);
+
+        Player.transform.Translate(translation);
+        Camera.transform.Translate(translation);
+
+        WorldManager.GenerateWorld(Player.transform.position);
     }
 }
--- bme-fon-valenara/Assets/_Scripts/WorldBuilder.cs
+++ bme-fon-valenara/Assets/_Scripts/WorldBuilder.cs
@@ -12,17 +12,19 @@ public class WorldBuilder : MonoBehaviour
     public GameObject CellPrefab;
     public int Width;
     public int Height;
-    private List<GameObject> Cells;
-
+    private Dictionary<(int x, int y), GameObject> _cells;
 
     public int RandomSeed;
     public TMP_InputField SeedInputField;
     public GameObject CellParent;
 
+    public PlayerMovementScript PlayerMovementScript;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GenerateWorld();
+        _cells = new Dictionary<(int x, int y), GameObject>();
+        GenerateWorldWithNewSeed();
     }
 
     public void SetSeed()
@@ -42,57 +44,91 @@ public class WorldBuilder : MonoBehaviour
         }
     }
 
+    public void GenerateWorldWithNewSeed()
+    {
+        SetSeed();
+        GenerateWorld();
+
+        PlayerMovementScript.MovePlayer(new Vector2(0, 0));
+    }
+
     public void GenerateWorld()
     {
         ClearCellHolder();
-        SetSeed();
 
-        Cells = new List<GameObject>();
         for (var w = 0; w < Width; w++)
         {
             for (var h = 0; h < Height; h++)
             {
-                var newCell = Instantiate(CellPrefab, new Vector3(w - Width / 2f, h - Height / 2f, 0), CellPrefab.transform.localRotation);
-
-                newCell.transform.SetParent(CellParent.transform);
-
-                var sx = (w + RandomSeed) / 20f;
-                var sy = (h + RandomSeed) / 20f;
-                //-477571
-                var perlinValue = Math.Clamp(Mathf.PerlinNoise(sx, sy), 0.0f, 1.0f);
-
-                var cellType = Math.Floor(perlinValue * 7.0f);
-
-                switch (cellType)
-                {
-                    case 0:
-                        newCell.AddComponent<PlainCellScript>();
-                        break;
-                    case 1:
-                        newCell.AddComponent<ForestCellScript>();
-                        break;
-                    case 2:
-                        newCell.AddComponent<WaterShallowCellScript>();
-                        break;
-                    case 3:
-                        newCell.AddComponent<WaterDeepCellScript>();
-                        break;
-                    case 4:
-                        newCell.AddComponent<DesertCellScript>();
-                        break;
-                    case 5:
-                        newCell.AddComponent<RockyCellScript>();
-                        break;
-                    case 6:
-                        newCell.AddComponent<MountainCellScript>();
-                        break;
-                    default:
-                        Debug.Log($"CellType not one of possible cases: {cellType}");
-                        break;
-                }
-
-                Cells.Add(newCell);
+                var position = new Vector2(w, h);
+                AddCell(position);
             }
+        }
+    }
+
+    public void GenerateWorld(Vector2 newPosition)
+    {
+        ClearCellHolder();
+        
+        for (var w = 0; w < Width; w++)
+        {
+            for (var h = 0; h < Height; h++)
+            {
+                var position = new Vector2(w + newPosition.x, h + newPosition.y);
+                AddCell(position);
+            }
+        }
+    }
+
+    private void AddCell(Vector2 position)
+    {
+        var x = (int)position.x;
+        var y = (int)position.y;
+
+        var newCell = Instantiate(CellPrefab, new Vector3(x - Width / 2f, y - Height / 2f, 0), CellPrefab.transform.localRotation);
+
+        newCell.transform.SetParent(CellParent.transform);
+
+        var sx = (x + RandomSeed) / 20f;
+        var sy = (y + RandomSeed) / 20f;
+        //-477571
+        var perlinValue = Math.Clamp(Mathf.PerlinNoise(sx, sy), 0.0f, 1.0f);
+
+        var cellType = (int)Math.Floor(perlinValue * 7.0f);
+
+        DetermineCell(newCell, cellType);
+
+        _cells.Add((x, y), newCell);
+    }
+
+    private void DetermineCell(GameObject cell, int cellType)
+    {
+        switch (cellType)
+        {
+            case 0:
+                cell.AddComponent<PlainCellScript>();
+                break;
+            case 1:
+                cell.AddComponent<ForestCellScript>();
+                break;
+            case 2:
+                cell.AddComponent<WaterShallowCellScript>();
+                break;
+            case 3:
+                cell.AddComponent<WaterDeepCellScript>();
+                break;
+            case 4:
+                cell.AddComponent<DesertCellScript>();
+                break;
+            case 5:
+                cell.AddComponent<RockyCellScript>();
+                break;
+            case 6:
+                cell.AddComponent<MountainCellScript>();
+                break;
+            default:
+                Debug.Log($"CellType not one of possible cases: {cellType}");
+                break;
         }
     }
 
@@ -102,6 +138,7 @@ public class WorldBuilder : MonoBehaviour
         {
             Destroy(child.gameObject);
         }
+        _cells.Clear();
     }
 
     // Update is called once per frame
